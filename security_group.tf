714e60cde7ccb1c9d039b4812a1f05d96a81876a
root <root@DESKTOP-F4AD9G2.localdomain>
2021-11-30 02:52:23 -0300

    update terraform

--- security_group.tf
+++ security_group.tf
@@ -129,7 +129,7 @@ resource "aws_security_group" "rds_sg" {
       from_port        = 3306
       to_port          = 3306
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = ["10.0.0.0/16"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
